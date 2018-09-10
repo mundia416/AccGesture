1715d75c9bf6e8896f761057f7955411ad7c1894
Mundia <fxmundia416@gmail.com>
2018-09-10 23:27:03 +0200

    set a custom start time for the stroke descriptor

--- accgesturelib/src/main/java/com/nosetrap/accgesturelib/Gesture.kt
+++ accgesturelib/src/main/java/com/nosetrap/accgesturelib/Gesture.kt
@@ -13,17 +13,18 @@ class Gesture(private val accService: AccessibilityService) {
 
     /**
      * click on the screen at the coordinates specified in @param point
-     *
+     ** @param startTime The time, in milliseconds, from the time the gesture starts to the
+     * time the stroke should start. Must not be negative.
      * @return true of gesture has been performed else false
      */
-    fun click(point: Point,gestureResultCallback: AccessibilityService.GestureResultCallback =
+    fun click(point: Point,strokeStartTime: Long = 10,gestureResultCallback: AccessibilityService.GestureResultCallback =
             object : AccessibilityService.GestureResultCallback() {}): Boolean {
         val builder = GestureDescription.Builder()
         val p = Path()
 
         p.moveTo(point.x.toFloat(), point.y.toFloat())
         p.lineTo(point.x.toFloat(), point.y.toFloat())
-        builder.addStroke(GestureDescription.StrokeDescription(p, 10, 200))
+        builder.addStroke(GestureDescription.StrokeDescription(p, strokeStartTime, 200))
 
         val gesture = builder.build()
 
@@ -33,15 +34,18 @@ class Gesture(private val accService: AccessibilityService) {
 
     /**
      * perform a scroll gesture from @param from to @param to
-     */
-    fun scroll(from: Point, to: Point, scrollDuration: Long = 1000,gestureResultCallback: AccessibilityService.GestureResultCallback =
+     * @param startTime The time, in milliseconds, from the time the gesture starts to the
+     * time the stroke should start. Must not be negative.
+     * */
+    fun scroll(from: Point, to: Point, scrollDuration: Long = 1000,strokeStartTime: Long = 0,
+               gestureResultCallback: AccessibilityService.GestureResultCallback =
             object : AccessibilityService.GestureResultCallback() {}): Boolean{
         val builder = GestureDescription.Builder()
         val p = Path()
 
         p.moveTo(from.x.toFloat(), from.y.toFloat())
         p.lineTo(to.x.toFloat(), to.y.toFloat())
-        builder.addStroke(GestureDescription.StrokeDescription(p, 0, scrollDuration))
+        builder.addStroke(GestureDescription.StrokeDescription(p, strokeStartTime, scrollDuration))
 
         val gesture = builder.build()
 
